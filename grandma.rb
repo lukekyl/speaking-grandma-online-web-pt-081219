46e2f56bd5e94d25abf2af71c547b86942d95efe
Kyle Luke <th.oldschool@gmail.com>
2019-08-14 20:22:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(string)
 new_string = string
 
 if new_string.upcase == string
-#  return "HUH?! SPEAK UP, SONNY!"
+  return "HUH?! SPEAK UP, SONNY!"
 #else if string == "I LOVE YOU GRANDMA!"
 #  return "I LOVE YOU TOO PUMPKIN!"
 #else
