2762b3e658026d671183bbaffb7769921f49366a
Kyle Luke <th.oldschool@gmail.com>
2019-08-14 20:26:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,9 +7,9 @@ new_string = string
 if string == "I LOVE YOU GRANDMA!"
   p "I LOVE YOU TOO PUMPKIN!"
 elsif new_string.upcase == string
-  p "HUH?! SPEAK UP, SONNY!"
-else
   p "NO, NOT SINCE 1938!"
+else
+  p "HUH?! SPEAK UP, SONNY!"
 end
 
 end
